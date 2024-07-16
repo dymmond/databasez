cb898478c3e509633c914951c18637f38d469e36
alex <devkral@web.de>
2024-07-16 17:17:20 +0200

    Changes: - make interfaces abstract

--- databasez/interfaces.py
+++ databasez/interfaces.py
@@ -1,74 +1,79 @@
+from __future__ import annotations
+
+__all__ = ["DatabaseBackend", "ConnectionBackend", "TransactionBackend"]
+
+
 import typing
-from collections.abc import Sequence
+from abc import ABC, abstractmethod
+from collections.abc import AsyncGenerator, Mapping, Sequence
 
 from sqlalchemy.sql import ClauseElement
 
 
-class DatabaseBackend:
-    async def connect(self) -> None:
+class Record(Sequence):
+    @property
+    def _mapping(self) -> Mapping:
         raise NotImplementedError()  # pragma: no cover
 
-    async def disconnect(self) -> None:
-        raise NotImplementedError()  # pragma: no cover
-
-    def connection(self) -> "ConnectionBackend":
+    def __getitem__(self, key: typing.Any) -> typing.Any:
         raise NotImplementedError()  # pragma: no cover
 
 
-class ConnectionBackend:
-    async def acquire(self) -> None:
-        raise NotImplementedError()  # pragma: no cover
+class TransactionBackend(ABC):
+    @abstractmethod
+    async def start(self, is_root: bool, extra_options: typing.Dict[str, typing.Any]) -> None: ...
 
-    async def release(self) -> None:
-        raise NotImplementedError()  # pragma: no cover
+    @abstractmethod
+    async def commit(self) -> None: ...
 
-    async def fetch_all(self, query: ClauseElement) -> typing.List["Record"]:
-        raise NotImplementedError()  # pragma: no cover
+    @abstractmethod
+    async def rollback(self) -> None: ...
 
-    async def fetch_one(self, query: ClauseElement) -> typing.Optional["Record"]:
-        raise NotImplementedError()  # pragma: no cover
+
+class ConnectionBackend(ABC):
+    @abstractmethod
+    async def acquire(self) -> None: ...
+
+    @abstractmethod
+    async def release(self) -> None: ...
+
+    async def fetch_all(self, query: ClauseElement) -> typing.List[Record]:
+        return [record async for record in self.iterate(query)]
+
+    @abstractmethod
+    async def fetch_one(self, query: ClauseElement) -> typing.Optional[Record]: ...
 
     async def fetch_val(self, query: ClauseElement, column: typing.Any = 0) -> typing.Any:
         row = await self.fetch_one(query)
         return None if row is None else row[column]
 
-    async def execute(self, query: ClauseElement) -> typing.Any:
-        raise NotImplementedError()  # pragma: no cover
+    @abstractmethod
+    async def execute(self, query: ClauseElement) -> typing.Any: ...
 
-    async def execute_many(self, queries: typing.List[ClauseElement]) -> None:
-        raise NotImplementedError()  # pragma: no cover
+    @abstractmethod
+    async def execute_many(self, queries: typing.List[ClauseElement]) -> None: ...
 
-    async def iterate(self, query: ClauseElement) -> typing.AsyncGenerator[typing.Mapping, None]:
+    @abstractmethod
+    async def iterate(self, query: ClauseElement) -> AsyncGenerator[Record, None]:
         raise NotImplementedError()  # pragma: no cover
         # mypy needs async iterators to contain a `yield`
         # https://github.com/python/mypy/issues/5385#issuecomment-407281656
         yield True  # pragma: no cover
 
-    def transaction(self) -> "TransactionBackend":
-        raise NotImplementedError()  # pragma: no cover
+    @abstractmethod
+    def transaction(self) -> TransactionBackend: ...
 
     @property
-    def raw_connection(self) -> typing.Any:
-        raise NotImplementedError()  # pragma: no cover
+    @abstractmethod
+    def raw_connection(self) -> typing.Any: ...
 
 
-class TransactionBackend:
-    async def start(
-        self, is_root: bool, extra_options: typing.Dict[typing.Any, typing.Any]
-    ) -> None:
-        raise NotImplementedError()  # pragma: no cover
+class DatabaseBackend(ABC):
+    @abstractmethod
+    async def connect(self) -> None: ...
 
-    async def commit(self) -> None:
-        raise NotImplementedError()  # pragma: no cover
+    @abstractmethod
+    async def disconnect(self) -> None: ...
 
-    async def rollback(self) -> None:
-        raise NotImplementedError()  # pragma: no cover
-
-
-class Record(Sequence):
-    @property
-    def _mapping(self) -> typing.Mapping:
-        raise NotImplementedError()  # pragma: no cover
-
-    def __getitem__(self, key: typing.Any) -> typing.Any:
-        raise NotImplementedError()  # pragma: no cover
+    @abstractmethod
+    def connection(self) -> ConnectionBackend: ...
