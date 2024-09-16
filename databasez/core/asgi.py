ea5238d9f3978d45c18d1f6a7fa4041157c7926d
alex <devkral@web.de>
2024-09-16 13:46:10 +0200

    fix wrong name

--- databasez/core/asgi.py
+++ databasez/core/asgi.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 from typing import TYPE_CHECKING, Any, Awaitable, Callable, Dict
 
 if TYPE_CHECKING:
-    from edgy.core.Database import Database
+    from databasez.core.Database import Database
 
 ASGIApp = Callable[
     [
